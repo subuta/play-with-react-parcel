8a71b71e14743306488ad9489f4ca6c5692ada1e
subuta <shunta.3110@gmail.com>
2019-04-28 22:22:11 +0900

    Add initial example.

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles.css'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        Hello
+        {this.props.name}
+      </div>
+    )
+  }
+}
+
+// boom
+var mountNode = document.getElementById('app')
+ReactDOM.render(<App name='Jane' />, mountNode)
