00e807caca20290ed3b1e19b4386266ad28d3177
subuta <shunta.3110@gmail.com>
2019-05-02 00:18:14 +0900

    Add hashmark example.

--- src/server/ssr.js
+++ src/server/ssr.js
@@ -6,11 +6,31 @@ import { Helmet } from 'react-helmet'
 
 import { source } from 'common-tags'
 
+import fs from 'fs'
+import path from 'path'
+import pkgDir from 'pkg-dir'
+
 import {
   getInitialProps,
   getInitialPropsVisitor,
   getScriptTag as printInitialPropsScript
 } from '/utils/initialProps'
+
+const dev = process.env.NODE_ENV !== 'production'
+
+const ROOT_DIR = pkgDir.sync()
+const ASSETS_JSON_PATH = path.resolve(ROOT_DIR, './dist/client/assets.json')
+
+// Load hashmark's assets-map JSON for production.
+let assetsJson = {}
+if (!dev && fs.existsSync(ASSETS_JSON_PATH)) {
+  assetsJson = require(ASSETS_JSON_PATH)
+}
+
+// Resolve hashed assets of production build.
+const resolveAssets = (filePath) => {
+  return assetsJson[filePath] ? assetsJson[filePath] : filePath
+}
 
 export default async (ctx, App) => {
   // Pre-render App for data fetching.
@@ -33,14 +53,14 @@ export default async (ctx, App) => {
             ${helmet.meta.toString()}
             ${helmet.link.toString()}
             
-            <link rel='stylesheet' href='/client.css'>
+            <link rel='stylesheet' href='${resolveAssets('client.css')}'>
             <meta charset='utf-8'>
         </head>
         <body ${helmet.bodyAttributes.toString()}>
             <div id='app'>${content}</div>
             
             ${printInitialPropsScript(ctx)}
-            <script src='/client.js'></script>
+            <script src='${resolveAssets('client.js')}'></script>
         </body>
     </html>
   `
